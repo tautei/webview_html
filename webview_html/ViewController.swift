f67ddc39965d1dc6d4c5714a3a910d18b0ef6303
monet <tautei.net@gmail.com>
2020-06-17 20:55:52 +0800

    Initial Commit

---
+++ webview_html/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webview_html
+//
+//  Created by monet-macbook-pro on 2020/6/17.
+//  Copyright © 2020 monet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ webview_htmlTests/webview_htmlTests.swift
@@ -0,0 +1,34 @@
+//
+//  webview_htmlTests.swift
+//  webview_htmlTests
+//
+//  Created by monet-macbook-pro on 2020/6/17.
+//  Copyright © 2020 monet. All rights reserved.
+//
+
+import XCTest
+@testable import webview_html
+
+class webview_htmlTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
